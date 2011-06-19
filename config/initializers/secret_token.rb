8813f72b76997257b0c11ce818540052bdf0c3f2
征矢 直記 <soyanaoki@susumuya-naoki-no-MacBook-Air.local>
2011-06-19 15:37:34 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aaa::Application.config.secret_token = 'c7e8aa54e51d0c47266f053dc2aa439a58fc8d3cf22fe91e0d1a2804dc87ee428ceddd85e8f783f115cf2883378aa63d78f8da006a93788c91de823c8daec4a6'
